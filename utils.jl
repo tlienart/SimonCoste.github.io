9c2681880a30feea3a949f2bfc618cdf2dca5556
Simon <costesimon@live.fr>
2021-03-24 20:36:32 +0100

    Added the blog part of the website.

--- utils.jl
+++ utils.jl
@@ -14,3 +14,75 @@ function lx_baz(com, _)
   # do whatever you want here
   return uppercase(brace_content)
 end
+
+
+function hfun_recentblogposts()
+  list = readdir("posts")
+  filter!(f -> endswith(f, ".md"), list)
+  dates = [stat(joinpath("blog", f)).mtime for f in list]
+  perm = sortperm(dates, rev=true)
+
+  io = IOBuffer()
+  write(io, """<ul class="blog-posts">""")
+
+  for (i, post) in enumerate(list[perm])
+    
+    ps  = splitext(post)[1]
+    write(io, "<li>")
+    url = "/posts/$ps/"
+    surl = strip(url, '/')
+    titlepost = pagevar(surl, :titlepost)
+    dateofpost = pagevar(surl, :date)
+    description = pagevar(surl, :abstract)
+
+    write(io, """<b><a href="$url">$titlepost</a></b> &nbsp; <i> ($dateofpost) </i>""")
+    write(io, """<li><i class="description">$description</i></li>""")
+  end
+  write(io, "</ul>")
+  return String(take!(io))
+end
+
+function hfun_blogposts()
+  today = Dates.today()
+  curyear = year(today)
+  curmonth = month(today)
+  curday = day(today)
+
+  list = readdir("posts")
+  filter!(endswith(".md"), list)
+  function sorter(p)
+      ps  = splitext(p)[1]
+      url = "/posts/$ps/"
+      surl = strip(url, '/')
+      pubdate = pagevar(surl, :published)
+      if isnothing(pubdate)
+          return Date(Dates.unix2datetime(stat(surl * ".md").ctime))
+      end
+      return Date(pubdate, dateformat"yyyy-mm-dd")
+  end
+  sort!(list, by=sorter, rev=true)
+
+  io = IOBuffer()
+  write(io, """<ul class="blog-posts">""")
+  for (i, post) in enumerate(list)
+      if post == "index.md"
+          continue
+      end
+      ps  = splitext(post)[1]
+      write(io, "<li><span><i>")
+      url = "/posts/$ps/"
+      surl = strip(url, '/')
+      title = pagevar(surl, :title)
+      pubdate = pagevar(surl, :published)
+      description = pagevar(surl, :rss)
+      if isnothing(pubdate)
+          date = "$curyear-$curmonth-$curday"
+      else
+          date = Date(pubdate, dateformat"yyyy-mm-dd")
+      end
+      write(io, """$date</i></span><b><a href="$url">$title</a></b>""")
+      write(io, """<li><i class="description">$description</i></li>""")
+  end
+  write(io, "</ul>")
+  return String(take!(io))
+end
